d64405ed7e2ce3106915a8ceece80622eecebaff
flocto <flocto@duck.com>
2025-01-30 23:51:11 -0600

    update template a bit more

--- templates/template.py
+++ templates/template.py
@@ -66,15 +66,94 @@ class ListNode(Node):
 class TreeNode(Node):
     def __init__(self, data, left = None, right = None, height = 0):
         super().__init__(data)
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
         self.height = height
 
 class TrieNode(Node):
-    def __init__(self, data, cnt = 0):
+    def __init__(self, data, cnt = 0, height = 0):
         super().__init__(data)
         self.children = {}
         self.cnt = cnt
+        self.height = height
+
+class PriorityQueue:
+    # min pq by default
+    def __init__(self, arr = None, key = lambda x: x):
+        self.key = key
+        self.arr = arr if arr else []
+        heapify(self.arr)
+    
+    def push(self, x):
+        heappush(self.arr, x)
+    
+    def pop(self):
+        return heappop(self.arr)
+    
+    def top(self):
+        return self.arr[0]
+
+    def __len__(self):
+        return len(self.arr)
+
+    def __bool__(self):
+        return bool(self.arr)
+    
+    def __repr__(self):
+        return f"PriorityQueue({self.arr})"
+    
+    def __str__(self):
+        return repr(self)
+
+    def __iter__(self):
+        return iter(self.arr)
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.height = [0] * n
+        self.size = [1] * n
+        self.num_sets = n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        self._union_by_height(x, y)
+        # self._union_by_size(x, y)
+
+    def _union_by_size(self, x, y):
+        x, y = self.find(x), self.find(y)
+        if x != y:
+            if self.size[x] < self.size[y]:
+                x, y = y, x
+            self.parent[y] = x
+            self.size[x] += self.size[y]
+            self.num_sets -= 1
+    
+    def _union_by_height(self, x, y):
+        x, y = self.find(x), self.find(y)
+        if x != y:
+            if self.height[x] < self.height[y]:
+                x, y = y, x
+            self.parent[y] = x
+            if self.height[x] == self.height[y]:
+                self.height[x] += 1
+            self.num_sets -= 1
+
+    def set_size(self, x):
+        return self.size[self.find(x)]
+
+    def __len__(self):
+        return self.num_sets
+
+    def __repr__(self):
+        return f"UnionFind({self.num_sets})"
+
+    def __str__(self):
+        return repr(self)
 
 # LOL what the hack is this 
 def topsort(graph):
@@ -91,6 +170,18 @@ def edges2graph(edges, directed=False):
         if not directed:
             graph[v].append(u)
     return graph
+
+def find_le(arr, x):
+    i = bisect_right(arr, x)
+    if i:
+        return i-1, arr[i-1]
+    return -1, None
+
+def find_ge(arr, x):
+    i = bisect_left(arr, x)
+    if i != len(arr):
+        return i, arr[i]
+    return -1, None
 
 input = FastIn(stdin).readline
 stdout = FastOut(stdout)
@@ -112,5 +203,5 @@ getalllines = lambda: [x.rstrip() for x in stdin.readlines()]
 
 # T = getnum()
 # for I in range(T):
-#     print(f"Case #{I+1}: ", end="")
+#     # print(f"Case #{I+1}: ", end="")
 #     # Your code here
